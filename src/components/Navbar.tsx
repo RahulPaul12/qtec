30d7eb23e00df08a1d46ec0a3255e2c06092378a
your_username <rahulpaul199914@gmail.com>
2025-07-14 20:08:46 +0600

    update route

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,20 @@
+import { Link } from "react-router";
 
 
 export default function Navbar ({setSidebarOpen}){
       const handleSidebar = () => {
-          setSidebarOpen(prev => !prev);
+          setSidebarOpen(open => !open);
       };
     return (
+        
     // <!--====================================
     //         HEADER PART START  
     //  =====================================-->
         <header className="header">
             <nav className="container flex justify-between items-center py-4 md:py-5">
-                <a href="/home">Qtec Solution</a>
+                <Link to="/">Qtec Solution</Link>
                 <ul className="text-sm font-medium ">
-                    <li className="hover:text-primary"><a href="/home">Home</a></li>
+                    <li className="hover:text-primary"><Link to="/">Home</Link></li>
                 </ul>
                 <button onClick={handleSidebar} className="flex items-center gap-2 font-medium text-sm bg-black/100 px-3 h-8 cursor-pointer rounded-full text-white">
                 <i className="fa-solid fa-cart-shopping text-xs "></i>
